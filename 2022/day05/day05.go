ed8e241036dbc4c4103700093d94b311c6541acd
Michael Leow <leow@sinarproject.org>
2022-12-09 23:21:38 +0800

    Summary: Minimal extract cellContent and pushing into stack for init

--- 2022/day05/day05.go
+++ 2022/day05/day05.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/bitfield/script"
-	"github.com/davecgh/go-spew/spew"
 	"strings"
 )
 
@@ -19,15 +18,59 @@ func Part1() {
 	if err != nil {
 		panic(err)
 	}
+	numofStack := 0
+	executeAction := false
 	for _, l := range data {
-		cols := strings.ReplaceAll(l, " ", "@")
-		//cols := strings.Split(l, " ")
-		spew.Dump(cols)
-		//fmt.Println("COLS:", len(cols))
-		// If see '1'; is a border; last number is x * n
-		// extract + skip 4 everytime ..
-
-		// Now in the instructions mode
+		if executeAction {
+			var bob string
+			fmt.Println("NOW START ACZtION ..")
+			return
+			// Now parse the actions .. instead of split to chars
+			// Now in the instructions mode
+			n, err := fmt.Scanf("boo %s", bob)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("NO chsrs:", n)
+		} else {
+			cols := strings.Split(l, "")
+			if numofStack > 0 {
+				// Check exit condition; a '1'
+				// If see '1'; is a border; last number is x * n
+				if cols[1] == "1" {
+					executeAction = true
+					continue
+				}
+				// Extract out the block based on numofStack
+				for i := 0; i < numofStack; i++ {
+					cellContent := strings.TrimSpace(cols[i*4+1])
+					// DEBUG
+					// fmt.Print("BLOCK:", cellContent)
+					// Push into your own stack .. if NOT empty
+					if cellContent != "" {
+						fmt.Println("PUSH", cellContent, "into Stack", i)
+					}
+				}
+				fmt.Println("=================>")
+			} else {
+				// extract + skip 4 everytime ..
+				numofStack = len(cols) % 4
+				// DEBUG
+				//spew.Dump(cols)
+				fmt.Println("COLS:", numofStack)
+				// Extract out the block based on numofStack
+				for i := 0; i < numofStack; i++ {
+					cellContent := strings.TrimSpace(cols[i*4+1])
+					// DEBUG
+					// fmt.Print("BLOCK:", cellContent)
+					// Push into your own stack .. if NOT empty
+					if cellContent != "" {
+						fmt.Println("PUSH", cellContent, "into Stack", i)
+					}
+				}
+				fmt.Println("=================>")
+			}
+		}
 	}
 
 	// From raw data; reverse = push into the stack
