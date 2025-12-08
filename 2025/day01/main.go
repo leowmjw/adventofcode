2965de5328c5723855c1d8bfb0a2547123262de2
Michael Leow <leowmjw@users.noreply.github.com>
2025-12-09 00:48:39 +0800

    Summary: Day 01 Pt2 fsiled

--- 2025/day01/main.go
+++ 2025/day01/main.go
@@ -12,11 +12,11 @@ func main() {
 
 	fmt.Println("Welcome to AOC 2025 Day01!!")
 	// Always starts at 50; with dial from 0 - 99 (100 items)
-	run("input.txt")
+	//part1("input.txt")
+	part2("input.txt")
 }
 
-func run(input string) {
-
+func part1(input string) {
 	// If action is 'L'; then compare if eq, less, more ..
 	s, serr := script.File(input).FilterLine(func(s string) string {
 		// Split command; Left is negative
@@ -70,10 +70,82 @@ func run(input string) {
 
 }
 
-func howFarFromHundred(direction string, current int) int {
-	if direction == "R" {
-		return 100 - current
+func part2(input string) {
+	// Difference is now it takes the mod results as additional count too ..
+	// If action is 'L'; then compare if eq, less, more ..
+	s, serr := script.File(input).FilterLine(func(s string) string {
+		// Split command; Left is negative
+		if s[0] == 'L' {
+			s = "-" + s[1:]
+		} else {
+			s = "+" + s[1:]
+		}
+		return s
+	}).Slice()
+	if serr != nil {
+		panic(serr)
 	}
-	// else is current ..
-	return current
+
+	// Find out how far from the edge; start of 50; it is 44 from edge both side
+	// howFarFromHundred ...
+	//spew.Dump(s)
+
+	count := 0
+	current := 50
+	//startedPositive := true
+	// Rule is simpler .. no need modulus
+	// If it reaches exactly 0; the increase the counter
+	for _, action := range s {
+		fmt.Println(action)
+		step, cerr := strconv.Atoi(action)
+		if cerr != nil {
+			panic(cerr)
+		}
+		// If currently positve; startedPositive == true
+		// If currently negative; startedPositive == false
+		//if current < 0 {
+		//	startedPositive = false
+		//}
+		current += step
+		if current == 0 || current == 100 {
+			count++
+		} else if current < 0 {
+			//if startedPositive == true {
+			count++
+			fmt.Println("LtoR Count: ", count)
+			//}
+		} else if current > 100 {
+			//if startedPositive == false {
+			count++
+			fmt.Println("RtoL Count: ", count)
+			//}
+		}
+		// HOw many times over 100
+		multiple := current / 100
+		if multiple < 0 {
+			multiple = multiple * -1
+		}
+		if multiple > 0 {
+			count += multiple
+			fmt.Println("Count: ", count)
+		}
+
+		fmt.Println("Multiple: ", multiple)
+		current = current % 100
+		if current < 0 {
+			// readjust dial
+			current += 100
+		}
+		fmt.Println("Current after step: ", current)
+		// now not just 0
+		// but count the left over from mod ..
+		//if current == 0 {
+		//	count++
+		//}
+		// In Go; can have begative mod .. but does not matter if divide
+		// by 100; any non-zero then add on ..
+		// If match == 0; the multiple == 1
+	}
+
+	fmt.Println("Count: ", count)
 }
--- 2025/day01/main_test.go
+++ 2025/day01/main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func Test_run(t *testing.T) {
+func Test_part1(t *testing.T) {
 	type args struct {
 		input string
 	}
@@ -16,7 +16,26 @@ func Test_run(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			run(tt.args.input)
+			part1(tt.args.input)
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		input string
+	}
+	// 50 + R50 (count+1) + R100 (count+1) + R100 (count+1)  + R9 =
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"happy", args{"part2.txt"}},
+		{"happy2", args{"part2a.txt"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			part2(tt.args.input)
 		})
 	}
 }
