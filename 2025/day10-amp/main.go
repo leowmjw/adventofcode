c4fa47d26c77a102effac8decadecb888cc251e0
Michael Leow <leowmjw@users.noreply.github.com>
2025-12-11 21:47:48 +0800

    Summary: Day10 Skeleton

---
+++ 2025/day10-amp/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+fmt.Println("Welcome to Day 10!!")
+run("input.txt")
+}
+
+func run(input string) {
+fmt.Println("run!!")
+}
--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 run:
-	@echo "Run Day 01.."
-	@cd 2025/day01 && go run main.go
+	@echo "Run Day 10.."
+	@cd 2025/day10-amp && go run main.go
