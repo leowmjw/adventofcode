2f85528c176a1f9e32d22ccda746b24a772b5723
Michael Leow <leowmjw@users.noreply.github.com>
2025-12-13 16:52:16 +0800

    Summary: Day02 Part1

--- 2025/day02/main.go
+++ 2025/day02/main.go
@@ -15,22 +15,44 @@ func main() {
 	// single digit; single digit ..
 	// Start length and final length .
 	// Samples: 11-22
-	part1("test.txt")
+	//part1("test.txt")
+	//part1("part1.txt")
+	part1("input.txt")
 }
 
-func findDupes(start, finish int) {
+func findDupes(start, finish int) int {
 	// Start from slice of 1 till .. max half ..
 	// Start with pair of 1; 2; 3 ..
 	fmt.Println("Finding dupes between", start, "and", finish)
+	invalidSum := 0
 	for i := start; i <= finish; i++ {
 		// Split into half by len which wiuld be max
-		fmt.Println(i)
+		// If len is not divisible by 2 .. skip ..
+		strascii := strconv.Itoa(i)
+		strlen := len(strascii)
+		// DEBUG
+		//fmt.Println(i, " LEN: ", strlen)
+		if strlen%2 != 0 {
+			//fmt.Println("Skipping ", i)
+			continue
+		}
+		left := strascii[:strlen/2]
+		right := strascii[strlen/2:]
+		fmt.Println("LEFT:", left, " RIGHT: ", right)
+		if left == right {
+			fmt.Println("Found dupes: ", strascii)
+			invalidSum += i
+		}
 	}
+	fmt.Println("Invalid Sum: ", invalidSum)
 	fmt.Println("Done ==========>>")
+
+	return invalidSum
 }
 
 func splitCount(line string) {
 	s := strings.Split(line, ",")
+	invalidSum := 0
 	for i := 0; i < len(s); i++ {
 		ids := strings.Split(s[i], "-")
 		// DEBUG
@@ -40,8 +62,10 @@ func splitCount(line string) {
 		if cerr != nil || cerr2 != nil {
 			panic(cerr)
 		}
-		findDupes(start, finish)
+		invalidSum += findDupes(start, finish)
+		fmt.Println("SUM: ", invalidSum)
 	}
+	fmt.Println("TOTAL Invalid Sum: ", invalidSum)
 }
 
 func part1(input string) {
