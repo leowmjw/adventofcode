08f7d260f1457906249a80f5f0716027a3aa3b64
Michael Leow <leowmjw@users.noreply.github.com>
2025-12-13 14:55:45 +0800

    Summary: Day 01 Pt2 - Rust ported over ..

--- 2025/day01-rust/src/main.rs
+++ 2025/day01-rust/src/main.rs
@@ -3,7 +3,9 @@
 fn main() {
 
     println!("Day01 - Rust");
-    part1();
+    // part1();
+    let my_str = include_str!("part1.txt");
+    part2(my_str);
 }
 
 
@@ -14,6 +16,45 @@ fn main() {
 //     let file = File::open(filename)?;
 //     Ok(io::BufReader::new(file).lines())
 // }
+
+fn part2(my_str: &str) {
+    // Below will be the more complex with steps more thsn 100++
+    // let my_str = include_str!("part2a.txt");
+
+    let mut count = 0;
+    let mut current = 50;
+
+    my_str.lines().for_each(
+        |line| {
+            // Extratc first char actiion .. maybe use the Match?
+            let cmd = &line[0..1];
+            // dbg!(cmd);
+            // Extract out the steps as int ..
+            let steps = line[1..].parse().unwrap();
+            let steps_size;
+            // If action is 'L'; then compare if eq, less, more ..
+            if cmd == "L" {
+                steps_size = -1;
+            } else {
+                steps_size = 1;
+            }
+
+            // For number of steps; simukate throuh each
+            // Once it recahes to 0 when modules then increase the counter
+            for _d in 1..=steps {
+                // dbg!(current);
+                current = ((current + steps_size) % 100 + 100 ) % 100;
+                if current == 0 {
+                    count = count + 1;
+                    println!("HIT!! {}", count);
+                }
+                // DEBUG
+                // println!("CURRENT: {} AFTER STEP: {}", current, steps_size);
+            };
+        }
+    );
+    println!("COUNT: {}", count);
+}
 
 fn part1() {
 
